f7d798809ef6755302f56c37487006de98ebe8d7
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-12-03 15:37:13 +0500

    Done

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.products.models import Product
+
+class Order(models.Model):
+    OrderStatus = [
+        ('panding', 'Panding'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('canceled', 'Canceled'),
+        ('refunded', 'Refunded'),
+    ]
+    PaymentStatus = [
+        ('panding', 'Panding'),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+        ('refunded', 'Refunded')
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True , null=True,
+        related_name='orders'
+    )
+    total_price = models.DecimalField(max_length=12, decimal_places=2)
+    status = models.CharField(choices=OrderStatus, default='panding')
+    payment_status = models.CharField(choices=PaymentStatus, default='panding')
+    address = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    def to_dict(self) -> dict:
+        return {
+            'id': self.pk,
+            'total_price': self.total_price,
+            'status': self.status,
+            'payment_status': self.payment_status,
+            'address': self.address,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            'items': [i.to_dict() for i in self.items.all()],
+            'user': self.user.to_dict()
+        }
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='order_items'
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    total = models.DecimalField(max_digits=12, decimal_places=2)
+    
+    def to_dict(self) -> dict:
+        return {
+            'id': self.pk,
+            'quantity': self.quantity,
+            'total': self.total
+        }
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+    
+    def to_dict(self) -> dict:
+        return {
+            'id': self.pk,
+            'name': self.name,
+            'description': self.description,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
+        
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveBigIntegerField(default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='products'
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.name
+    
+    def to_dict(self) -> dict:
+        return {
+             "id": self.pk,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "stock": self.stock,
+            "is_active": self.is_active,
+            "category": self.category.to_dict() if self.category else None,
+            "images": [img.to_dict() for img in self.images.all()],
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_pics/')
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+    alt_text = models.CharField(max_length=256, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def to_dict(self) -> dict:
+        return {
+            "id": self.pk,
+            "url": self.image.url,
+            "alt_text": self.alt_text,
+            "created_at": self.created_at.isoformat()
+        }
+        
+    class Meta:
+        ordering = ['created_at']
+        
+    def __str__(self):
+        return f"Image for {self.product.name}"
